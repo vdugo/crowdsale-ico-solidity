e33e121bd752bfa682c10fb0444d0559e72aeac9
Vincent <v3rdugo@yahoo.com>
2023-06-05 18:32:33 -0700

    loading blockchain data and visual loading spinner working

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,96 @@
 import { Container } from "react-bootstrap"
 import { ethers } from "ethers"
 import { useEffect, useState } from "react"
+
 // Components
 import Navigation from "./Navigation"
 import Info from "./Info"
+import Loading from "./Loading"
+
+// ABIs
+import TOKEN_ABI from '../abis/Token.json'
+import CROWDSALE_ABI from '../abis/Crowdsale.json'
+
+import config from '../config.json'
+
+// local deployed on Hardhat contract addresses
+const TOKEN_ADDRESS = config[31337].token.address
+const CROWDSALE_ADDRESS = config[31337].crowdsale.address
 
 
 function App()
 {
+    const [provider, setProvider] = useState(null)
+
+    const [crowdsale, setCrowdsale] = useState(null)
+
+    const [accountBalance, setAccountBalance] = useState(null)
+
+    const [price, setPrice] = useState(0)
+    const [maxTokens, setMaxTokens] = useState(0)
+    const [tokensSold, setTokensSold] = useState(0)
+
     const [account, setAccount] = useState(null)
+
+    const [isLoading, setIsLoading] = useState(true)
 
     const loadBlockchainData = async () =>
     {
+        // initialize the provider
         const provider = new ethers.providers.Web3Provider(window.ethereum)
-        console.log(provider)
+        setProvider(provider)
 
+        // initialize contracts
+        const token = new ethers.Contract(TOKEN_ADDRESS, TOKEN_ABI, provider)
+        console.log(token.address)
+
+        const crowdsale = new ethers.Contract(CROWDSALE_ADDRESS, CROWDSALE_ABI, provider)
+        console.log(crowdsale.address)
+        setCrowdsale(crowdsale)
+
+        // set account
         const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
         const account = ethers.utils.getAddress(accounts[0])
-        
         setAccount(account)
+
+        const accountBalance = ethers.utils.formatUnits(await token.balanceOf(account), 18)
+        setAccountBalance(accountBalance)
+
+        const price = ethers.utils.formatUnits(await crowdsale.price(), 18)
+        setPrice(price)
+        const maxTokens = ethers.utils.formatUnits(await crowdsale.maxTokens(), 18)
+        setMaxTokens(maxTokens)
+        const tokensSold = ethers.utils.formatUnits(await crowdsale.tokensSold(), 18)
+        setTokensSold(tokensSold)
+
+        setIsLoading(false)
     }
 
     useEffect(() =>
     {
-        loadBlockchainData()
-    })
+        if (isLoading)
+        {
+            loadBlockchainData()
+        }
+    }, [isLoading])
 
     return(
         <Container>
             <Navigation />
+            {isLoading ? 
+            (
+                <Loading />
+            )
+            :
+            (
+                <p className="text-center"><strong>Current price: {price} ETH</strong></p>
+            )
+            }
             <hr />
             {
                 account &&
                 (
-                    <Info account={account} />
+                    <Info account={account} accountBalance={accountBalance} />
                 )
             }
         </Container>
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,8 +1,9 @@
-const Info = ({account}) =>
+const Info = ({account, accountBalance}) =>
 {
     return(
         <div className="my-3">
             <p><strong>Account:</strong> {account}</p>
+            <p>Tokens owned: {accountBalance}</p>
         </div>
     )
 }
